5cca8fdf7c34897e3e581e0d24736d60c0864a3a
Alex Schmith <alexanderschmith@protonmail.com>
2024-02-21 15:08:29 -0500

    Fixed compile issues

--- src/commands.h
+++ src/commands.h
@@ -1,22 +1,26 @@
 #pragma once
 
-#include "BinaryNinja.h"
+#include <binaryninjaapi.h>
+
+
+namespace BN = BinaryNinja;
+
 
 /**
  * Plugin commands.
  */
 class Commands {
     public:
-        static void jump_end_of_block(BinaryViewRef, uint64_t);
-        static void jump_start_of_block(BinaryViewRef, uint64_t);
-        static void jump_next_left_block(BinaryViewRef, uint64_t);
-        static void jump_next_right_block(BinaryViewRef, uint64_t);
-        static void jump_left_block(BinaryViewRef, uint64_t);
-        static void jump_right_block(BinaryViewRef, uint64_t);
-        static void jump_up_block(BinaryViewRef, uint64_t);
+        static void jump_end_of_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_start_of_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_next_left_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_next_right_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_left_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_right_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
+        static void jump_up_block(BN::Ref<BN::BinaryView>BinaryViewRef, uint64_t);
 
         /**
         * Register plugin commands for all one-shot actions.
         */
-        static void registerCommands();
+        static void register_commands();
 };

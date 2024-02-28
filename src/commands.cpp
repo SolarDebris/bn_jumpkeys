87350093a1f0ed9bc4302c1f1e45fddd9e29fbf4
Alex Schmith <alexanderschmith@protonmail.com>
2024-02-28 15:21:40 -0500

    Update commands.cpp

--- src/commands.cpp
+++ src/commands.cpp
@@ -2,30 +2,36 @@
 
 // Jump to the end of current basic block
 void Commands::jump_end_of_block(BinrayViewRef bv){
-
+    bv::Navigate();
 }
 
 void Commands::jump_start_of_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
 void Commands::jump_next_left_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
 void Commands::jump_next_right_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
 void Commands::jump_left_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
 void Commands::jump_right_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
 void Commands::jump_up_block(BinaryViewRef bv){
+    bv::Navigate();
 
 }
 
@@ -34,7 +40,7 @@ void Commands::register_commands(){
 
     BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the end of current block", "", Commands::jump_end_of_block);
     BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the start of current block", "", Commands::jump_start_of_block);
-    BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the block down and left of current block", "" Commands::jump_next_left_block);
+    BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the block down and left of current block", "", Commands::jump_next_left_block);
     BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the block down and right of the current block", "",  Commands::jump_next_right_block);
     BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the block left of the current block", "", Commands::jump_left_block);
     BinaryNinja::PluginCommand::register_for_address("Binja Jumpkeys\\Jump to the block right of the current block", "", Commands::jump_right_block);
